e4e7a62539bac21ce579781555995d33593e63de
emnaboukhris <79046370+emnaboukhris@users.noreply.github.com>
2021-04-09 12:40:44 +0200

    initial commit

---
+++ forEach.js
@@ -0,0 +1,31 @@
+const somme =()=>{
+console.log(argument); 
+operation="+"; 
+operation === "+"? (total = 0) : (total= 1) ; 
+args.forEach((x)=>{
+if(operation==="+"){
+    total += x ; 
+
+} else if(operation === "*"){
+    total *= x ; 
+}
+
+}) ; 
+console.log(total) ; 
+
+} ; 
+somme(1,2,3,4,5) ; 
+t.splice()
+function f(){
+var x=2 ; 
+var y=4
+ ;
+  console.log(somme(x,y)) ; 
+  var somme =function(a,b)
+{console.log(a+b);}
+}; 
+f(); 
+
+
+
+}
